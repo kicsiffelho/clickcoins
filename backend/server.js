18afc66d5825036779ce49ae37b75ae4a9feb968
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2024-10-04 12:18:09 +0200

    Trying clerk auth

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../frontend'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     root: './frontend',
     build: {
-    outDir: 'dist',
+    outDir: './dist',
     },
 });
