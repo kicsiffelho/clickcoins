2f35ee4a393f167f184f92ea02e4c0a88ab66c6d
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2024-10-05 12:19:25 +0200

    Revert "Adding clerk"

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,6 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static('../frontend'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'reg.html'));
-});
-
-app.get('/welcome', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'welcome.html'));
-});
-
-app.get('/game', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'game.html'));
-});
-
-app.get('/store', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'store.html'));
-});
-
-app.get('/score', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'score.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(3000, () => console.log('Server running on port 3000'));
