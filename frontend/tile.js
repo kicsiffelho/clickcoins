53c3c2fbbbe4245106786d4f65c51ab9959b44e1
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2024-10-05 13:23:01 +0200

    Fixing vite

--- frontend/tile.js
+++ frontend/tile.js
@@ -1,9 +1,9 @@
-function changeBackgroundColor(color) {
+export function changeBackgroundColor(color) {
   localStorage.setItem("background", color);
   location.href = "main.html";
 }
 
-function setBackgroundColor() {
+export function setBackgroundColor() {
   const gameAreaColor = localStorage.getItem("background");
   if (gameAreaColor) {
     const gameArea = document.getElementById("game-area");
