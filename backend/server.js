8dfb962aaf686a7936b8bd95c81afd08a923edb0
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2024-10-17 00:41:52 +0200

    Debugging clerk

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const scoreRoute = require('./routes/scoreRoute');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.use('/api', scoreRoute);
