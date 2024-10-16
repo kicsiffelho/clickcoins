e9307290bc16dc79ff598e7f434ee5af80dd6062
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2024-10-17 00:07:19 +0200

    Debugging clerk

--- frontend/clerkInit.js
+++ frontend/clerkInit.js
@@ -3,11 +3,9 @@ import { Clerk } from "@clerk/clerk-js";
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 const clerk = new Clerk(clerkPubKey);
 
-window.clerk = clerk;
-
 async function initClerk() {
     await clerk.load();
-
+    console.log('Clerek initalized:', clerk);
     const userButtonDiv = document.getElementById("user-button");
     if (userButtonDiv) {
         clerk.mountUserButton(userButtonDiv);
