c9c395ff3b5da6045941b3e0964367644708236b
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2024-10-17 22:30:57 +0200

    Fixed currencyDisplay

--- frontend/js/currencyDisplay.js
+++ frontend/js/currencyDisplay.js
@@ -1,5 +1,6 @@
 function updateCurrencyDisplay(currencyAmount) {
     const currencyDisplay = document.getElementById('currency-display');
+    currencyDisplay.innerHTML = '';
 
     const coinImage = document.createElement('img');
     coinImage.src = new URL('../assets/coin.png', import.meta.url).href;
@@ -7,7 +8,7 @@ function updateCurrencyDisplay(currencyAmount) {
     coinImage.style.width = '40px';
     coinImage.style.height = '40px';
 
-    const amountDisplay = document.createElement('div');
+    const amountDisplay = document.createElement('span');
     amountDisplay.textContent = currencyAmount;
     amountDisplay.style.marginLeft = '20px';
 
