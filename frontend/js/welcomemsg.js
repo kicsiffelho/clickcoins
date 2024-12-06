f795b13517db24d25b652671153a463c2b897ef3
kicsiffelho <41196477+kicsiffelho@users.noreply.github.com>
2024-12-06 20:58:39 +0100

    Fixed username display

--- frontend/js/welcomemsg.js
+++ frontend/js/welcomemsg.js
@@ -1,7 +1,7 @@
 function displayWelcomeMsg() {
     const username = clerk.user.username;
     const welcomeMsgDiv = document.getElementById('welcome-msg');
-    welcomeMsgDiv.textContent = `<h2 id="welcome-msg">Hi ${username}!</h2>`
+    welcomeMsgDiv.textContent = `Hi ${username}!`
 }
 
 export { displayWelcomeMsg };
